53cb91fbff12d2812de5b92738a41709c04c6b86
sg0311903 <dawid.fremel@sabre.com>
2022-09-12 23:46:56 +0200

    Fix

--- src/components/difelmark.jsx
+++ src/components/difelmark.jsx
@@ -8,14 +8,14 @@ export const Difelmark = (props) => {
                             <h2>DIFELMARK</h2>
                             <p><b>Oferuje profesjonalne usługi w zakresie zwalczania szkodników oraz zabezpieczenia obiektów przed pojawieniem się ich lub powrotem.
                                 Skuteczność naszych działań to efekt zasady, że każde rozwiązanie jest dobierane indywidualnie do potrzeb Klienta.</b></p>
-                            <p>
+                            <p><br/>
                                 Występowanie szkodników niesie ze sobą poważne zagrożenia dla zdrowia ludzi oraz straty szczególnie w produkcji spożywczej. Dysponujemy zarówno wiedzą, jak i najnowszą technologią pozwalającą na skuteczną i  kompleksową ochronę obiektów i terenów. Podstawową zasadą naszego działania jest założenie, że kontrola i zwalczanie szkodników muszą iść w parze z bezpieczeństwem dla ludzi i środowiska naturalnego. </p>
                             <p>
                                 Działamy zgodnie z wytycznymi systemów GMP (Dobrej Praktyki Produkcyjnej), GHP (Dobrej Praktyki Higienicznej) oraz zaawansowanym systemem HACCP (Analiza Zagrożeń i Krytyczne Punkty Kontrolne), którego zastosowanie ma na celu zapobieganie występowaniu oraz uniemożliwienie dostępu szkodnikom na teren zabezpieczony poprzez ograniczenie warunków, które mogą sprzyjać ich bytowaniu i rozwojowi. </p>
                             <p>
                                 Stosujemy środki najlepszych producentów, dostępne jedynie dla firm specjalistycznych, posiadających wszelkie wymagane atesty i certyfikaty.</p>
                             <p><b> Należymy do Polskiego Stowarzyszenia Pracowników DDD. </b></p>
-                            <p>
+                            <p><br/>
                                 Unikatowym elementem naszej oferty jest zarówno oczyszczanie prywatnych posesji, jak i terenów użyteczności publicznej z uciążliwych gryzoni tj. zwalczanie kretów, nornic i innych gryzoni niszczących trawniki, rabaty z krzewami, kwiatami itp. Tak więc możemy śmiało powiedzieć, że mamy środek na kreta,nornicę oraz jej krewniaków  i wiemy, jak się ich pozbyć z miejsc, gdzie są nieproszonymi gośćmi.</p>
                             <p>
                                 Jednocześnie naszą ambicją jest umożliwienie Klientom samodzielnego skutecznego zwalczania szkodników poprzez dostarczenie skutecznych i bezpiecznych preparatów, dostępnych dotychczas tylko dla profesjonalistów z uprawnieniami.</p>
